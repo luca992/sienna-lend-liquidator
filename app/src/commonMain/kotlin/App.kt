b85df6ec626ac4f8a207cba3ff4498d8b45fc781
Luca Spinazzola <mspinluca@gmail.com>
2023-11-23 03:13:10 -0500

    add drop down to change lend market

--- app/src/commonMain/kotlin/App.kt
+++ app/src/commonMain/kotlin/App.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import co.touchlab.kermit.Logger
 import com.ionspin.kotlin.bignum.integer.BigInteger
+import com.ionspin.kotlin.bignum.integer.toBigInteger
 import io.eqoty.secretk.client.SigningCosmWasmClient
 import io.eqoty.secretk.wallet.DirectSigningWallet
 import io.getenv
@@ -26,37 +28,69 @@ fun App(viewModel: AppViewModel) {
     var buttonText by remember { mutableStateOf("Query for loans to liquidate!") }
     val balance by remember {
         derivedStateOf {
-            viewModel.balance[viewModel.selectedLendMarket.underlyingAssetId] ?: BigInteger.ZERO
+            viewModel.balance[viewModel.selectedLendMarket.value.underlyingAssetId] ?: -1.toBigInteger()
         }
     }
     val loans by remember {
         derivedStateOf {
-            viewModel.marketToLoans[viewModel.selectedLendMarket] ?: emptyList()
+            viewModel.marketToLoans[viewModel.selectedLendMarket.value] ?: emptyList()
         }
     }
+    LaunchedEffect(Unit) {
+        // trigger the wallet balance to update by
+        viewModel.init()
+    }
+    var selectLendMarketDropdownExpanded by remember { mutableStateOf(false) }
 
     val coroutineScope = rememberCoroutineScope()
-
     MaterialTheme {
         Scaffold(topBar = {
             TopAppBar(title = {
                 Text("Liquidator")
             }, actions = {
-                Text("Balance: $balance ${viewModel.selectedLendMarket.underlyingAssetId.snip20Symbol}")
+                OutlinedButton(onClick = {
+                    selectLendMarketDropdownExpanded = !selectLendMarketDropdownExpanded
+                }) {
+                    Text("Select Lend Market")
+                }
+                DropdownMenu(
+                    expanded = selectLendMarketDropdownExpanded,
+                    onDismissRequest = {
+                        selectLendMarketDropdownExpanded = false
+                    },
+                ) {
+                    viewModel.allLendMarkets.forEach { lendMarket ->
+                        DropdownMenuItem({
+                            Text(lendMarket.underlyingAssetId.snip20Symbol)
+                        }, onClick = {
+                            coroutineScope.launch {
+                                viewModel.setSelectedLendMarket(lendMarket)
+                            }
+                            selectLendMarketDropdownExpanded = false
+                        })
+                    }
+                }
             })
         }) { innerPadding ->
             Column(
                 modifier = Modifier.padding(innerPadding).fillMaxSize()
             ) {
-                Button(onClick = {
-                    buttonText = "Working..."
-                    coroutineScope.launch {
-                        viewModel.getLoans()
-                        buttonText = "Query for loans to liquidate!"
-                    }
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(onClick = {
+                        buttonText = "Working..."
+                        coroutineScope.launch {
+                            viewModel.getLoans()
+                            buttonText = "Query for loans to liquidate!"
+                        }
 
-                }) {
-                    Text(buttonText)
+                    }) {
+                        Text(buttonText)
+                    }
+                    Text("Balance: $balance ${viewModel.selectedLendMarket.value.underlyingAssetId.snip20Symbol}")
                 }
                 LazyColumn {
                     items(loans) { loan ->
@@ -112,8 +146,7 @@ fun LoanCard(loan: Loan, onClickLiquidate: suspend () -> Unit) {
                     fontWeight = FontWeight.Bold
                 )
                 Spacer(modifier = Modifier.height(4.dp))
-                val loanCollateralSnip20Symbol =
-                    loan.candidate.marketInfo.underlyingAssetId.snip20Symbol
+                val loanCollateralSnip20Symbol = loan.candidate.marketInfo.underlyingAssetId.snip20Symbol
                 Text("Total Amount Owed (Payable): ${loan.candidate.totalPayable} $loanCollateralSnip20Symbol")
                 Text("Clamped Amount Owed (Payable): ${loan.candidate.payable.toPlainString()} $loanCollateralSnip20Symbol")
                 Text("Clamped USD Value Of Payable: ${loan.candidate.payableUsd.toPlainString()}")
@@ -138,7 +171,6 @@ fun LoanCard(loan: Loan, onClickLiquidate: suspend () -> Unit) {
         }
     }
 }
-
 
 
 fun getRepositoryWithDirectSigningWallet(): Repository {
--- app/src/commonMain/kotlin/AppViewModel.kt
+++ app/src/commonMain/kotlin/AppViewModel.kt
@@ -1,3 +1,6 @@
+import androidx.compose.runtime.mutableStateOf
+import datalayer.functions.updateUserBalance
+import types.LendOverseerMarket
 import types.Loan
 
 class AppViewModel(val repo: Repository, private val liquidator: Liquidator) {
@@ -5,13 +8,23 @@ class AppViewModel(val repo: Repository, private val liquidator: Liquidator) {
     val balance = repo.runtimeCache.userBalance
     val marketToLoans = repo.runtimeCache.marketToLoans
     val allLendMarkets = repo.runtimeCache.lendOverseerMarkets
-    var selectedLendMarket = repo.runtimeCache.lendOverseerMarkets.first()
+    var selectedLendMarket = mutableStateOf(repo.runtimeCache.lendOverseerMarkets.first())
+
+    suspend fun setSelectedLendMarket(lendMarket: LendOverseerMarket) {
+        selectedLendMarket.value = lendMarket
+        repo.updateUserBalance(lendMarket)
+    }
+
     suspend fun getLoans() {
-        liquidator.updateLiquidations(selectedLendMarket)
+        liquidator.updateLiquidations(selectedLendMarket.value)
     }
 
     suspend fun liquidate(loan: Loan) {
         liquidator.liquidate(loan)
+    }
+
+    suspend fun init() {
+        repo.updateUserBalance(selectedLendMarket.value)
     }
 
 }
